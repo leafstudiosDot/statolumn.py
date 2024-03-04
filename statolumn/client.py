7178c13d7e80b9677de9b103c852f92ea69f895f
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2024-03-04 10:34:20 +0800

    Access Token init

--- statolumn/client.py
+++ statolumn/client.py
@@ -1,8 +1,19 @@
 import requests
 
 class Client:
-    def __init__(self, clientid):
+    access_token = ""
+
+    def __init__(self, clientid, secret):
+        self.clientid = clientid
+        self.secret = secret
         pass
+
+    def setAccessToken(self, token):
+        self.access_token = token
+        pass
+
+    def getAccessToken(self):
+        return self.access_token
 
     def getUserDetails(self):
         pass
